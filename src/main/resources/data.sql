a5888456a50166e4578bec3fb10d6e8d809d787f
lukechenluwang@gmail.com <luke.w@bmyg.com.au>
2019-08-06 12:20:19 +1000

    added database seeder for toppings

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
 INSERT INTO topping (id, description, image_url, name, unit_price) VALUES (1, 'This is Anchovies', '', 'Anchovies', 2);
+INSERT INTO topping (id, description, image_url, name, unit_price) VALUES (2, 'This is Spring Onion', '', 'Spring Onion', 0.5);
+INSERT INTO topping (id, description, image_url, name, unit_price) VALUES (3, 'This is Indian Butter Sauce', '', 'Indian Butter Sauce', 1);
+INSERT INTO topping (id, description, image_url, name, unit_price) VALUES (4, 'This is Capsicum', '', 'Capsicum', 1.5);
+INSERT INTO topping (id, description, image_url, name, unit_price) VALUES (5, 'This is Seasoned Chicken', '', 'Seasoned Chicken', 3);
